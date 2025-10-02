336107120325fd3a9580cb43660d2de5a6747a4f
Daima <muhiyabenjamin@gmail.com>
2025-10-02 22:40:27 +0100

    feat(hero-section): add a profile picture and style
    Adds a profile picture with a new style and improves the aesthetics of the hero section.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -5,6 +5,18 @@ export default function HeroSection() {
   return (
     <section id="about" className="max-w-[1200px] mx-auto px-5 mb-20">
       <div className="bg-[#121212] rounded-xl p-10 flex flex-col lg:flex-row justify-between items-center gap-20">
+        <div className="relative">
+          <div className="rounded-full w-[400px] h-[400px] bg-gradient-to-br from-gray-600 to-gray-800 rounded-[32px] relative overflow-hidden">
+            <ImageWithFallback
+              src="/daima-m.jpg"
+              alt="daima muhiya"
+              className="rounded-full w-full h-full object-cover"
+            />
+          </div>
+          <div className="absolute bottom-7 right-20 w-14 h-14 rounded flex items-center justify-center">
+            <Code2 className="w-10 h-10 text-[#057A55]" />
+          </div>
+        </div>
         <div className="flex-1 max-w-[541px]">
           <div className="mb-6">
             <p className="text-[#F98080] text-base mb-3 font-mono">
@@ -24,18 +36,6 @@ export default function HeroSection() {
               <img src="/icons/css.png" alt="Javascript" className="w-6 h-6" />
             </div>
             <span className="text-[#6B7280] font-mono">... et plus encore</span>
-          </div>
-        </div>
-        <div className="relative">
-          <div className="w-[500px] h-[500px] bg-gradient-to-br from-gray-600 to-gray-800 rounded-[32px] relative overflow-hidden">
-            <ImageWithFallback
-              src="/daima-m.jpg"
-              alt="daima muhiya"
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="absolute bottom-7 right-20 w-14 h-14 bg-[#057A55] rounded flex items-center justify-center">
-            <Code2 className="w-10 h-10 text-[#0D0D0D]" />
           </div>
         </div>
       </div>
